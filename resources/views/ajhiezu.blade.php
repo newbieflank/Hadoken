a5115cabef449bcffeaa39a6652b1f6246ce28cb
Ajhiezu <ajhiezudesign@gmail.com>
2025-02-05 09:45:00 +0700

    ajhiezu page

---
+++ resources/views/ajhiezu.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <div class="row">
+        <div class="col-5">
+            <ul>
+                <li>Minuman</li>
+                <li>Makanan</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
